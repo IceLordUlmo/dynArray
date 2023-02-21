2bf7d9096fbb54b5cf20eaace1de7be0cbe1017d
Benjaminross75 <mrbenross@gmail.com>
2023-02-21 16:08:06 -0600

    dyn-array-solve

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,43 +1,65 @@
 class DynamicArray {
 
   constructor(defaultSize = 4) {
+       this.capacity = defaultSize;
+       this.length = 0;
+       this.data = new Array(this.capacity)
 
-    // Your code here
   }
 
   read(index) {
+   return this.data[index]
 
-    // Your code here
   }
 
   push(val) {
+    if(this.length === this.capacity) this.resize()
+  this.data[this.length] = val;
+  this.length++;
 
-    // Your code here
   }
 
 
   pop() {
+ if(this.length === 0) return;
+ this.length--;
+ return this.data[this.length]
 
-    // Your code here
   }
 
   shift() {
-
-    // Your code here
+ if(this.length === 0) return;
+ let returnValue = this.data[0];
+ this.length--
+ for(let i = 0; i < this.length; i++){
+  this.data[i] = this.data[i + 1];
+ }
+  return returnValue
   }
 
   unshift(val) {
-
-    // Your code here
+    if(this.length === this.capacity) this.resize()
+   for(let i = this.length; i >= 0; i--){
+    this.data[i + 1] = this.data[i];
+   }
+    this.data[0] = val
+    this.length++
   }
 
   indexOf(val) {
-
-    // Your code here
+  for(let i = 0; i < this.length; i++){
+    if(this.data[i] === val) return i
+  }
+ return -1
   }
 
   resize() {
-
+    this.capacity *=2;
+   let newArray = new Array(this.capacity);
+   for(let i = 0; i < this.length; i++){
+    newArray[i] = this.data[i];
+   }
+   this.data = newArray
     // Your code here
   }
 
